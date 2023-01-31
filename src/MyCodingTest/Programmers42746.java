0c7d1503c59bb892439f4c02533e4d5021bdabfb
zack8361 <zack6353@naver.com>
2023-01-31 23:20:38 +0900

    가장큰수(Lv.2)
    프로그래머스 : 가장큰수(Lv.2)

--- src/MyCodingTest/Programmers42746.java
+++ src/MyCodingTest/Programmers42746.java
@@ -1,11 +1,31 @@
 package MyCodingTest;
 
+import java.util.Arrays;
+
 /**
  * 정렬
- * 가장 큰 수 (Lv.1)
+ * 가장 큰 수 (Lv.2)
  */
 public class Programmers42746 {
     public static void main(String[] args) {
+        int[] numbers ={6,10,2};
+        System.out.println(solution(numbers));
+    }
 
+    private static String solution(int[] numbers) {
+
+        String[] arr = new String[numbers.length];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = String.valueOf(numbers[i]);
+        }
+        if(arr[0].equals("0") && arr[1].equals("0")){
+            return "0";
+        }
+        Arrays.sort(arr,(o1, o2) -> (o2+o1).compareTo(o1+o2));
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<arr.length; i++){
+            sb.append(arr[i]);
+        }
+        return String.valueOf(sb);
     }
 }
