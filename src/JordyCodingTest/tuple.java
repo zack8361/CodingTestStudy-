57b912a0d164b157c038716911b178c021a66521
zack8361 <zack6353@naver.com>
2023-03-01 19:20:20 +0900

    tuple 오늘 꼭 풀어보기
    tuple 오늘 꼭 풀어보기

--- src/JordyCodingTest/tuple.java
+++ src/JordyCodingTest/tuple.java
@@ -18,37 +18,37 @@ public class tuple {
             }
         }
 
-//        int[] arr = new int[str.length];
-//        for(int i=0; i<arr.length; i++){
-//            arr[i] = Integer.parseInt(str[i]);
-//        }
+////        int[] arr = new int[str.length];
+////        for(int i=0; i<arr.length; i++){
+////            arr[i] = Integer.parseInt(str[i]);
+////        }
+////
+//        String[] str = num.split(",");
+//        HashMap<Integer,Integer> map = new HashMap<>();
 //
-        String[] str = num.split(",");
-        HashMap<Integer,Integer> map = new HashMap<>();
-
-//        for(int i=0; i<str.length; i++){
-//            if(map.get(str[i]) == null){
-//                map.put(str[i],1);
-//            }
-//            else {
-//                map.put(str[i],map.get(str[i])+1);
-//            }
-//        }
+////        for(int i=0; i<str.length; i++){
+////            if(map.get(str[i]) == null){
+////                map.put(str[i],1);
+////            }
+////            else {
+////                map.put(str[i],map.get(str[i])+1);
+////            }
+////        }
+////
+////        int[] answer = new int[map.size()];
+////        int index = 0;
+////        for(String i: map.keySet()){
+////            answer[index++] = map.get(i);
+////        }
+////        Integer[] answer2 = Arrays.stream(answer).boxed().toArray(Integer[]::new);
+////        Arrays.sort(answer2, Collections.reverseOrder());
+////        System.out.println(Arrays.toString(answer2));
+////
+////        int[] realAns = new int[answer.length];
+////        for(int i=0; i<answer.length; i++){
 //
-//        int[] answer = new int[map.size()];
-//        int index = 0;
-//        for(String i: map.keySet()){
-//            answer[index++] = map.get(i);
-//        }
-//        Integer[] answer2 = Arrays.stream(answer).boxed().toArray(Integer[]::new);
-//        Arrays.sort(answer2, Collections.reverseOrder());
-//        System.out.println(Arrays.toString(answer2));
-//
-//        int[] realAns = new int[answer.length];
-//        for(int i=0; i<answer.length; i++){
-
-//
-//        }
+////
+////        }
 
         int[] answer = {};
         return answer;
