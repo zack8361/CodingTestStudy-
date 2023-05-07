31fd25c413b0749c3123df17923bf004210a10ab
zack8361 <zack6353@naver.com>
2023-05-07 20:36:27 +0900

    부분수열의합 왜안되지?
    부분수열의합 왜안되지?

--- src/SWEAD3/부분수열의합.java
+++ src/SWEAD3/부분수열의합.java
@@ -10,23 +10,43 @@ public class 부분수열의합 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int tc = sc.nextInt();
-        for(int t=1; t<=tc; t++){
+        for(int t=1; t<=tc; t++) {
             int N = sc.nextInt(); //배열 크기 입력
             K = sc.nextInt(); // target 입력.
-            int[]arr = new int[N];
-            int[]output = new int[N];
+            int[] arr = new int[N];
+
             visited = new boolean[N];
             // 배열 값 입력.
-            for(int i=0; i<arr.length; i++){
+            for (int i = 0; i < arr.length; i++) {
                 arr[i] = sc.nextInt();
             }
-
+            int[] output = new int[arr.length];
             for(int i=0; i<arr.length; i++){
-                dfs(arr,output,0,i+1);
+                dfs(arr,0,0,i+1);
             }
-            System.out.println(answer);
+            System.out.println("#" +t +" "+answer);
         }
     }
-    
+
+    private static void dfs(int[] arr, int start, int depth, int r) {
+        if(depth == r){
+            int sum = 0;
+            for(int i=0; i<arr.length; i++){
+                if(visited[i]){
+                    sum+=arr[i];
+                }
+            }
+            if(sum == K){
+                answer++;
+            }
+        }
+        for(int i=start; i<arr.length; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                dfs(arr,i+1,depth+1,r);
+                visited[i] = false;
+            }
+        }
     }
+
 }
