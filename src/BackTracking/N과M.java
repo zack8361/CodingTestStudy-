d030130b27dc8c1642e489c87c62392055433d1c
zack8361 <zack6353@naver.com>
2023-06-02 13:22:50 +0900

    N과 M문제 풀어보기
    N과 M문제 풀어보기

--- src/BackTracking/N과M.java
+++ src/BackTracking/N과M.java
@@ -8,6 +8,24 @@ public class N과M {
     private static boolean[] visited;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        visited = new boolean[M];
+        dfs(N,M,"");
+    }
+
+    private static void dfs(int n, int m, String s) {
+        if(s.length() == 4){
+            System.out.println(s);
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if(visited[i+1]) {
+                visited[i+1] = true;
+                dfs(n, m, s + " " + i);
+                visited[i+1] = false;
+            }
+        }
     }
 }
