eee1602d7199679a2f7945ed96e32f36078a2298
zack8361 <zack6353@naver.com>
2023-06-13 20:37:09 +0900

    queue / stack FiFO, LIFO 알아보기
    queue / stack FiFO, LIFO 알아보기

---
+++ src/BackTracking/제일작은수.java
@@ -0,0 +1,28 @@
+package BackTracking;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
+public class 제일작은수 {
+    // queue -> 선입 선출 -> FIFO 의 자료구조 형태를 가지고있다.
+    public static void main(String[] args) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(1);
+        queue.add(2);
+        System.out.println("queue = " + queue);
+        System.out.println("queue = " + queue.poll());
+        stack();
+    }
+
+    private static void stack(){
+
+        // Stack -> 후입 선출 -> LIFO 의 자료구조 형태를 가지고있다. 박스를 여러개 차곡차곡 쌓아두고 위에서 부터 스택대로 뽑는 느낌.
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        System.out.println("stack = " + stack);
+        System.out.println("stack = " + stack.pop());
+    }
+}
