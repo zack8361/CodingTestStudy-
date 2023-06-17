a14c7f4846ae38fe5db1cfd9783cb62fccaecc22
zack8361 <zack6353@naver.com>
2023-06-17 19:30:14 +0900

    call by ref
    call by ref

--- src/callByValue/callByReference.java
+++ src/callByValue/callByReference.java
@@ -6,8 +6,10 @@ public class callByReference {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        for (int i = 0; i < N; i++) {
-            System.out.println(i);
+        int sum = 0;
+        for (int i = 0; i <= N; i++) {
+            sum += i;
         }
+        System.out.println("sum = " + sum);
     }
 }
